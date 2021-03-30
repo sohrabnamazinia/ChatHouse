0035c3451e784a60c7bd7ceda27c38914a76dbef
ali <ali.farahat78@gmail.com>
2021-03-30 15:02:40 +0430

    add suggest search

---
+++ SAM Backend/SAM Backend/Controllers/SearchController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SAM_Backend.Models;
+using SAM_Backend.Services;
+using SAM_Backend.Utility;
+using SAM_Backend.ViewModels.Account;
+
+namespace SAM_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class SearchController : ControllerBase
+    {
+        #region Fields
+
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly ILogger<SearchController> logger;
+        private readonly IJWTService jWTService;
+        private readonly IDataProtectionProvider dataProtectionProvider;
+        private readonly AppDbContext context;
+        private readonly IDataProtector protector;
+
+        #endregion
+
+        public SearchController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ILogger<SearchController> logger, IJWTService jWTService, IDataProtectionProvider dataProtectionProvider, AppDbContext context)
+        {
+            #region Instantiation
+
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+            this.jWTService = jWTService;
+            this.dataProtectionProvider = dataProtectionProvider;
+            this.context = context;
+            this.protector = dataProtectionProvider.CreateProtector(DataProtectionPurposeStrings.UserIdQueryString);
+
+            #endregion
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<ICollection<AppUserViewModel>>> Suggest([FromQuery] PaginationParameters pagination)
+        {
+            List<AppUser> suggestedUsers = await context.Users.OrderByDescending(x => x.Followers.Count).Skip((pagination.PageNumber - 1) * (pagination.PageSize)).Take(pagination.PageSize).ToListAsync();
+
+            return suggestedUsers.Select(x => new AppUserViewModel(x)).ToList();
+        }
+    }
+}
---
+++ SAM Backend/SAM Backend/Utility/PaginationParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAM_Backend.Utility
+{
+    public class PaginationParameters
+    {
+        const int MaxPageSize = 20;
+        public int PageNumber { get; set; } = 1;
+        private int _PageSize { get; set; } = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _PageSize;
+            }
+            set
+            {
+                _PageSize = (value <= MaxPageSize) ? value : MaxPageSize;
+            }
+        }
+    }
+}
