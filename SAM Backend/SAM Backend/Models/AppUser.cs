0d3988d72432d210718d38ae5b0d8fe372280bec
ali <ali.farahat78@gmail.com>
2021-03-30 15:00:40 +0430

    add lazy loading

--- SAM Backend/SAM Backend/Models/AppUser.cs
+++ SAM Backend/SAM Backend/Models/AppUser.cs
@@ -8,8 +8,8 @@ namespace SAM_Backend.Models
 {
     public class AppUser : IdentityUser
     {
-        public List<AppUser> Followers { get; set; }
-        public List<AppUser> Followings { get; set; }
+        public virtual List<AppUser> Followers { get; set; }
+        public virtual List<AppUser> Followings { get; set; }
         public virtual Interests Interests { get; set; }
     }
 }
