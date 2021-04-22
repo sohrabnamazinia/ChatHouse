50923ea97166eef587371abbd5ca22f76d1ac1b8
unknown <sohrabsn7@yahoo.com>
2021-04-22 20:41:02 +0430

    unittest login-logout

---
+++ SAM Backend/SAM.Tests/MockedServices/FakeUserManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using SAM_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SAM.Tests.MockedServices
+{
+    public class FakeUserManager : UserManager<AppUser>
+    {
+        public FakeUserManager()
+            : base(new Mock<IUserStore<AppUser>>().Object,
+              new Mock<IOptions<IdentityOptions>>().Object,
+              new Mock<IPasswordHasher<AppUser>>().Object,
+              new IUserValidator<AppUser>[0],
+              new IPasswordValidator<AppUser>[0],
+              new Mock<ILookupNormalizer>().Object,
+              new Mock<IdentityErrorDescriber>().Object,
+              new Mock<IServiceProvider>().Object,
+              new Mock<ILogger<UserManager<AppUser>>>().Object)
+        { }
+    }
+}
