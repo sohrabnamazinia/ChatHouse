8bc744d8f63819d149b7afd8678774bbcda07fc1
unknown <sohrabsn7@yahoo.com>
2021-05-06 13:19:34 +0430

    edit getRoom user viewmodel

---
+++ SAM Backend/SAM Backend/ViewModels/Room/AppUserRoomViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAM_Backend.ViewModels.Room
+{
+    public class AppUserRoomViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
