4ae3807941d6890fa8329082aa936fb501bc25e0
Sohrab <sohrabsn7@yahoo.com>
2021-05-09 09:17:09 +0430

    complete login tests

--- SAM Backend/SAM.Tests/AccountTests/LoginTest.cs
+++ SAM Backend/SAM.Tests/AccountTests/LoginTest.cs
@@ -93,7 +93,136 @@ namespace SAM.Tests
             #endregion
         }
 
-        
+        [TestMethod]
+        public void Login_InvalidModelState_400()
+        {
+            #region Setup
+            mockUserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new AppUser());
+            mockSigninManager.Setup(x => x.PasswordSignInAsync(It.IsAny<AppUser>(), It.IsAny<string>(), It.IsAny<bool>(), true)).ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
+            mockJWTHandler.Setup(x => x.GenerateToken(It.IsAny<AppUser>())).Returns("JWT");
+            var trueCode = 400;
+            #endregion Setup
+
+            #region create controller
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = mockContext.Object
+            };
+            var controller = new AccountController(mockUserManager.Object, mockSigninManager.Object, mockAccountLogger.Object, mockJWTHandler.Object, mockDPProvider.Object, context, MockMinio.Object)
+            {
+                ControllerContext = controllerContext,
+                Url = mockUrl.Object
+            };
+            controller.ModelState.AddModelError(string.Empty, "Invalid Model State");
+            #endregion
+
+            #region test model
+            var model = new LoginViewModel()
+            {
+                // Invalid email format
+                Identifier = "testemail.com",
+                Password = "testpassword",
+                IsEmail = true
+            };
+            #endregion
+
+            #region login
+            Task<ActionResult> result = controller.Login(model);
+            int code = getResponse_400(result);
+            #endregion
+
+            #region Assertion
+            Assert.AreEqual(trueCode, code);
+            #endregion
+        }
+
+        [TestMethod]
+        public void Login_NotConfirmedEmail_401()
+        {
+            #region Setup
+            mockUserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new AppUser());
+            mockSigninManager.Setup(x => x.PasswordSignInAsync(It.IsAny<AppUser>(), It.IsAny<string>(), It.IsAny<bool>(), true)).ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.NotAllowed);
+            mockJWTHandler.Setup(x => x.GenerateToken(It.IsAny<AppUser>())).Returns("JWT");
+            var trueCode = 401;
+            #endregion Setup
+
+            #region create controller
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = mockContext.Object
+            };
+            var controller = new AccountController(mockUserManager.Object, mockSigninManager.Object, mockAccountLogger.Object, mockJWTHandler.Object, mockDPProvider.Object, context, MockMinio.Object)
+            {
+                ControllerContext = controllerContext,
+                Url = mockUrl.Object
+            };
+            #endregion
+
+            #region test model
+            var model = new LoginViewModel()
+            {
+                // Email not confirmed
+                Identifier = "testemail@yahoo.com",
+                Password = "testpassword",
+                IsEmail = true
+            };
+            #endregion
+
+            #region login
+            Task<ActionResult> result = controller.Login(model);
+            int code = getResponse_401(result);
+            #endregion
+
+            #region Assertion
+            Assert.AreEqual(trueCode, code);
+            #endregion
+        }
+
+        [TestMethod]
+        public void Login_LockedoutAccount_423()
+        {
+            #region Setup
+            mockUserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new AppUser());
+            mockSigninManager.Setup(x => x.PasswordSignInAsync(It.IsAny<AppUser>(), It.IsAny<string>(), It.IsAny<bool>(), true)).ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.LockedOut);
+            mockJWTHandler.Setup(x => x.GenerateToken(It.IsAny<AppUser>())).Returns("JWT");
+            var trueCode = 423;
+            #endregion Setup
+
+            #region create controller
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = mockContext.Object
+            };
+            var controller = new AccountController(mockUserManager.Object, mockSigninManager.Object, mockAccountLogger.Object, mockJWTHandler.Object, mockDPProvider.Object, context, MockMinio.Object)
+            {
+                ControllerContext = controllerContext,
+                Url = mockUrl.Object
+            };
+            #endregion
+
+            #region test model
+            var model = new LoginViewModel()
+            {
+                // User is locked out
+                Identifier = "LockedoutUser@yahoo.com",
+                Password = "testPassword",
+                IsEmail = true
+            };
+            #endregion
+
+            #region login
+            Task<ActionResult> result = controller.Login(model);
+            int code = getResponse_423(result);
+            #endregion
+
+            #region Assertion
+            Assert.AreEqual(trueCode, code);
+            #endregion
+        }
+
+
+
+
 
 
         #region get responses
@@ -107,6 +236,27 @@ namespace SAM.Tests
         public int getResponse_404(Task<ActionResult> result)
         {
             NotFoundObjectResult actionResult = (NotFoundObjectResult)result.Result;
+            var code = actionResult.StatusCode;
+            return code.Value;
+        }
+
+        public int getResponse_400(Task<ActionResult> result)
+        {
+            BadRequestObjectResult actionResult = (BadRequestObjectResult)result.Result;
+            var code = actionResult.StatusCode;
+            return code.Value;
+        }
+
+        public int getResponse_401(Task<ActionResult> result)
+        {
+            UnauthorizedObjectResult actionResult = (UnauthorizedObjectResult)result.Result;
+            var code = actionResult.StatusCode;
+            return code.Value;
+        }
+
+        public int getResponse_423(Task<ActionResult> result)
+        {
+            ObjectResult actionResult = (ObjectResult)result.Result;
             var code = actionResult.StatusCode;
             return code.Value;
         }
